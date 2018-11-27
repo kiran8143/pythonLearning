3db09f9fb5a306c81e68374aad81bf6cc636583a
全晟斌 <wk0012345@naver.com>
2018-11-27 11:23:34 +0900

    python learning dictionary

--- lesson8.py
+++ lesson8.py
@@ -6,3 +6,40 @@ for i in b:
     print(i)
 print("--------------------")
 print(b[:len(b)])
+
+#dic
+x = {'a':10 , 'b':3 , 'c' :3}
+
+print(x)
+print(x['a'])
+
+x['d'] = 10
+
+print(x)
+
+x['d'] = -1
+
+print(x['d'])
+
+print(len(x))
+
+x.update(a = 100 , b = 200)
+
+print(x)
+
+x['z'] = 111
+
+print(x)
+print(x.pop('z'))
+print("---------------")
+print(x)
+print(x.popitem())
+
+print("---------------")
+print(x)
+print(x.items())
+#print(x.values())
+xValue = list(x.values())
+print(xValue)
+x.setdefault("xx",222)
+print(x)
